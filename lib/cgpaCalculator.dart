9f4241b8a68f937a8691138d446514a6d37111d5
rokayaraka <raka23105101503@diu.edu.bd>
2025-05-05 19:38:31 +0600

    cgpa calculator

--- lib/cgpaCalculator.dart
+++ lib/cgpaCalculator.dart
@@ -1,18 +1,23 @@
-
-
+import 'package:diu_result/const.dart';
 import 'package:flutter/material.dart';
 
 class Cgpacalculator extends StatefulWidget {
   const Cgpacalculator({super.key});
 
-  
-
   @override
   State<Cgpacalculator> createState() => _CgpacalculatorState();
 }
 
-
 class _CgpacalculatorState extends State<Cgpacalculator> {
+  NewRow init = NewRow();
+  List<Widget> courseData = [];
+  List<NewRow> courseList = [];
+  @override
+  void initState() {
+    super.initState();
+    courseData.add(init.getRow());
+    courseList.add(init);
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -21,64 +26,133 @@ class _CgpacalculatorState extends State<Cgpacalculator> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: courseData,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                  flex: 3,
-                  child: TextField(
-                    controller: TextEditingController(),
-                    decoration: InputDecoration(
-                      labelText: 'Course Title',
-                      labelStyle: TextStyle(color: Colors.black),
-                      hintText: 'Enter Course Title',
-                      hintStyle: TextStyle(color: Colors.grey,
-                      fontSize: 10,
-                      ),
-                      border: OutlineInputBorder(),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        courseData.add(SizedBox(
+                          height: 20,
+                        ));
+                        NewRow course = NewRow();
+                        courseList.add(course);
+                        courseData.add(course.getRow());
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shadowColor: Colors.black,
+                      elevation: 10,
+                    ),
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.green,
+                    ),
+                    label: Text("Add Course",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )
                     ),
                   ),
                 ),
-                SizedBox(width: 10),
+                SizedBox(width: 20,),
                 Expanded(
-                  flex: 2,
-                  child: TextField(
-                    controller: TextEditingController(),
-                    decoration: InputDecoration(
-                      labelText: 'Credit',
-                      labelStyle: TextStyle(color: Colors.black),
-                      hintText: 'Enter Course Credit',
-                      hintStyle: TextStyle(color: Colors.grey,
-                      fontSize: 10,
-                      ),
-                      border: OutlineInputBorder(),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        if(courseData.length>1)
+                        {
+                        courseData.removeLast();
+                        courseData.removeLast();
+                        courseList.removeLast();
+                        }
+                        else{
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text("Alert!"),
+                              content: Text("You must have at least one course."),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("OK"),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.delete,color: Colors.white,),
+                    label: Text("Remove Course",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )
                     ),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  flex: 2,
-                  child: TextField(
-                    controller: TextEditingController(),
-                    decoration: InputDecoration(
-                      labelText: 'Grade point',
-                      labelStyle: TextStyle(color: Colors.black),
-                      hintText: 'Enter Course Grade point',
-                      hintStyle: TextStyle(color: Colors.grey,
-                      fontSize: 10,
-                      ),
-                      border: OutlineInputBorder(),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red.shade300,
+                      shadowColor: Colors.black,
+                      elevation: 10,
+                      
                     ),
                   ),
                 ),
               ],
             ),
-            
-          ],
-          ),
-        ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: (){
+                double totalCredit = 0;
+                double totalGradePoint = 0;
+                for(int i=0;i<courseList.length;i++)
+                {
+                  double currentCredit = courseList[i].creditControler.text.isEmpty?0:double.parse(courseList[i].creditControler.text);
+                  double currentGradePoint = courseList[i].gradeControler.text.isEmpty?0:double.parse(courseList[i].gradeControler.text);
+                  totalCredit += currentCredit;
+                  totalGradePoint += currentCredit*currentGradePoint;
+                }
+                double cgpa = totalGradePoint/totalCredit;
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text("Your SGPA"),
+                    content: Text("Your SGPA is ${cgpa.toStringAsFixed(2)}"),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("OK"),
+                      ),
+                    ],
+                  ),
+                );
 
+            }, 
+            child: Text("Calculate CGPA",),
+            ),
+          ],
+        ),
+      ),
     );
   }
-  
 }
--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,6 @@
 
+import 'package:flutter/material.dart';
+
 final semester=[
   "spring",
   "summer",
@@ -8,3 +10,118 @@ final years = List.generate(
   DateTime.now().year - 2022 + 1, // Calculate the number of years
   (index) => (2022 + index).toString(), // Generate years starting from 2022
 );
+
+class NewRow {
+    final courseControler=TextEditingController();  
+    final creditControler=TextEditingController();
+    final gradeControler=TextEditingController();
+  Row getRow(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 3,
+            child: TextField(
+              controller: courseControler,
+              decoration: InputDecoration(
+                labelText: 'Course Title',
+                labelStyle: TextStyle(color: Colors.black),
+                hintText: 'Enter Course Title',
+                hintStyle: TextStyle(color: Colors.grey,
+                fontSize: 10,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            flex: 2,
+            child: TextField(
+              controller: creditControler,
+              decoration: InputDecoration(
+                labelText: 'Credit',
+                labelStyle: TextStyle(color: Colors.black),
+                hintText: 'Enter Course Credit',
+                hintStyle: TextStyle(color: Colors.grey,
+                fontSize: 10,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            flex: 2,
+            child: TextField(
+              controller: gradeControler,
+              decoration: InputDecoration(
+                labelText: 'Grade point',
+                labelStyle: TextStyle(color: Colors.black),
+                hintText: 'Enter Course Grade point',
+                hintStyle: TextStyle(color: Colors.grey,
+                fontSize: 10,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      );
+  }
+}
+  
+
+// Row init=Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Expanded(
+//                     flex: 3,
+//                     child: TextField(
+                      
+//                       decoration: InputDecoration(
+//                         labelText: 'Course Title',
+//                         labelStyle: TextStyle(color: Colors.black),
+//                         hintText: 'Enter Course Title',
+//                         hintStyle: TextStyle(color: Colors.grey,
+//                         fontSize: 10,
+//                         ),
+//                         border: OutlineInputBorder(),
+//                       ),
+//                     ),
+//                   ),
+//                   SizedBox(width: 10),
+//                   Expanded(
+//                     flex: 2,
+//                     child: TextField(
+//                       controller: TextEditingController(),
+//                       decoration: InputDecoration(
+//                         labelText: 'Credit',
+//                         labelStyle: TextStyle(color: Colors.black),
+//                         hintText: 'Enter Course Credit',
+//                         hintStyle: TextStyle(color: Colors.grey,
+//                         fontSize: 10,
+//                         ),
+//                         border: OutlineInputBorder(),
+//                       ),
+//                     ),
+//                   ),
+//                   SizedBox(width: 10),
+//                   Expanded(
+//                     flex: 2,
+//                     child: TextField(
+//                       controller: TextEditingController(),
+//                       decoration: InputDecoration(
+//                         labelText: 'Grade point',
+//                         labelStyle: TextStyle(color: Colors.black),
+//                         hintText: 'Enter Course Grade point',
+//                         hintStyle: TextStyle(color: Colors.grey,
+//                         fontSize: 10,
+//                         ),
+//                         border: OutlineInputBorder(),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               );
+
